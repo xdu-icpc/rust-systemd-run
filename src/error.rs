1aa9d2219f8b4ffe795491123ed9fb64e2037a08
Xi Ruoyao <xry111@xry111.site>
2022-08-17 10:10:11 +0800

    use thiserror to simplify error handling code

--- src/error.rs
+++ src/error.rs
@@ -1,58 +1,33 @@
-use std::fmt::Display;
 use zbus::zvariant::OwnedValue;
 
 /// The error type of `systemd_run`.
 ///
 /// The various errors that can be returned by this crate.
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     /// An error connecting to D-Bus.
+    #[error("cannot connect to dbus: {0}")]
     DBusConnectionFail(zbus::Error),
     /// Invalid D-Bus path.
+    #[error("cannot get a valid dbus path: {0}")]
     DBusInvalidPath(zbus::zvariant::Error),
     /// An error subscribing to D-Bus `PropertiesChanged` signal.
+    #[error("cannot start listening property change events: {0}")]
     ListenPropertyChangeFail(zbus::Error),
     /// An error parsing the changed properties from the D-Bus
     /// `PropertiesChanged` signal.
+    #[error("cannot parse the property change event: {0}")]
     ParsePropertyChangeFail(zbus::Error),
     /// An error quering one property of a unit.
+    #[error("cannot query the property: {0}")]
     QueryPropertyFail(zbus::fdo::Error),
     /// An error calling systemd to start the transient unit.
+    #[error("cannot start the transient service: {0}")]
     StartFail(zbus::Error),
     /// An error attempting to calculate the time usage of a service.
+    #[error("cannot calculate {0} time usage: t0 = {1:?}, t1 = {2:?}")]
     TimeUsageFail(&'static str, Box<OwnedValue>, Box<OwnedValue>),
 }
 
 /// Alias for a [Result][std::result::Result] with the error type [Error].
 pub type Result<T> = std::result::Result<T, Error>;
-
-impl Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
-        match self {
-            Self::DBusConnectionFail(e) => {
-                write!(f, "cannot connect to dbus: {}", e)
-            }
-            Self::DBusInvalidPath(e) => {
-                write!(f, "cannot get a dbus path: {}", e)
-            }
-            Self::StartFail(e) => {
-                write!(f, "cannot start transient service: {}", e)
-            }
-            Self::ListenPropertyChangeFail(e) => {
-                write!(f, "cannot listen property change events: {}", e)
-            }
-            Self::ParsePropertyChangeFail(e) => {
-                write!(f, "cannot parse property changes: {}", e)
-            }
-            Self::QueryPropertyFail(e) => {
-                write!(f, "cannot parse property changes: {}", e)
-            }
-            Self::TimeUsageFail(what, t0, t1) => {
-                write!(f, "cannot calculate {} time usage: ", what)?;
-                write!(f, "t0 = {:?}, t1 = {:?}", t0, t1)
-            }
-        }
-    }
-}
-
-impl std::error::Error for Error {}
