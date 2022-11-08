3888e591021f65deb6c4a4f336a0689e33b19166
Xi Ruoyao <xry111@xry111.site>
2022-11-08 17:06:59 +0800

    build: fix a needless borrow in build script

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use std::process::Command;
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let st = Command::new("make")
-        .args(&["-C", "test-aux"])
+        .args(["-C", "test-aux"])
         .arg(&format!("OUT_DIR={}", out_dir))
         .status()
         .unwrap();
